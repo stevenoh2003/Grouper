7a6e916d01cf59e383f5a39f150b652183f6c509
SoySoy4444 <sshoinkyuraa.08.04@hotmail.co.jp>
2020-07-06 18:33:32 +0900

    Create User model

--- app/models.py
+++ app/models.py
@@ -1,2 +1,14 @@
-#!/usr/bin/env python
+from app import db, login_manager #import db and login_manager variables from __init__.py
+from flask_login import UserMixin #video 6
 
+@login_manager.user_loader
+def load_user(user_id):
+	return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+	id = db.Column(db.Integer, primary_key=True)
+	email = db.Column(db.String(120), unique=True, nullable=False)
+	password = db.Column(db.String(60), nullable=False)
+
+	def __repr__(self):
+		return f"User({self.email}, {self.image_file})"
