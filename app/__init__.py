983e2b1025ce1421e23fd8cc652df562bc46c45d
SoySoy4444 <sshoinkyuraa.08.04@hotmail.co.jp>
2021-06-22 11:24:31 +0900

    Added deployment libraries

--- app/__init__.py
+++ app/__init__.py
@@ -5,16 +5,21 @@ from flask_login import LoginManager
 from dotenv import load_dotenv
 import os
 from flask_mail import Mail
+from flask_talisman import Talisman
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
 load_dotenv()
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY") #protect against cookie manipulation and attacks
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+migrate = Migrate(app, db)
+talisman = Talisman(app, content_security_policy=None)
+
 login_manager = LoginManager(app)
 login_manager.login_view = "login" #pass in the function name of the login route
 login_manager.login_message_category = "info" #Styles "Please log in to view this page" with Bootstrap "info" class
